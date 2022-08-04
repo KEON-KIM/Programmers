35132ae29af2202c5d49fd963d7aae4f84b78052
keon-kim <srihweb@naver.com>
2022-08-04 17:27:47 +0900

    #20220804

--- level3/diskController.cpp
+++ level3/diskController.cpp
@@ -1,47 +1,50 @@
-#include <string>
 #include <vector>
 #include <iostream>
 #include <queue>
-
-#define MAX 501
-#define INF 1000000
-#define FOR(i, n) for(int i = 0; i < n; i++)
-
+#include <algorithm>
+ 
 using namespace std;
+ 
+typedef vector<int> v;
 
-bool visited[MAX] = {false, };
-int dfs(int curTime, int answer, vector<vector<int>> &jobs)
-{
-	int minValue = INF;
-	int idx = -1;
-	FOR(i, jobs.size())
-	{
-		if(jobs[i][0] <= curTime && !visited[i])
-		{
-			if(minValue > (jobs[i][1] + curTime) - jobs[i][0])
-			{
-				idx = i;
-				minValue = (jobs[i][1] + curTime) - jobs[i][0];
-			}
-		}
-	}
-	if(idx == -1) return answer;
-	
-	visited[idx] = true;
-	answer = dfs(curTime + jobs[idx][1], answer + minValue, jobs);
-	return answer;
+struct cmp {
+    bool operator()(v a, v b) {
+        return a[1] > b[1];
+    }
+};
+ 
+int solution(vector<v> jobs) {
+    int answer = 0,  i = 0, curTime = 0;
+    sort(jobs.begin(), jobs.end());
+    priority_queue<v, vector<v>, cmp> pQue;
+
+    while (i < jobs.size() || !pQue.empty()) 
+    {
+        if (jobs.size() > i && curTime >= jobs[i][0]) 
+        {
+            pQue.push(jobs[i++]);
+            continue;
+        }
+        if (!pQue.empty()) 
+        {
+            curTime += pQue.top()[1];
+            answer += curTime - pQue.top()[0];
+            pQue.pop();
+            cout << curTime << endl;
+        }
+        else
+            curTime = jobs[i][0];
+    }
+
+    return answer / jobs.size();
 }
-int solution(vector<vector<int>> jobs) {
-    int answer = 0;
-    answer = dfs(0, 0, jobs);
-
-    return answer/ jobs.size();
-}
-
 int main()
 {
-	vector<vector<int>> jobs
-		= {{0, 3}, {1, 9}, {2, 6}};
+	vector<v> jobs
+		// = {{0, 3}, {1, 9}, {2, 6}};
+	// = {{0, 3}, {2, 9}, {3, 6}};
+	= { {15, 34}, {24, 10}, {28, 39}, {43, 20}, {37, 5}, {47, 22}, {20, 47}, {15, 2}, {35, 43}, {26, 1}};
+	 // = {{0, 6}, {0, 8}, {7, 1}};
 	cout << solution(jobs);
 	return 0;
 }
