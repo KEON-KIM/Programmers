72d14c2fa9c5af97149fe4cd63b9663a643c59b4
keon-kim <srihweb@naver.com>
2022-08-26 18:16:53 +0900

    #20220826

--- level2/friends4block.cpp
+++ level2/friends4block.cpp
@@ -3,119 +3,128 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 #define MAX 31
 #define FOR(i, n) for(int i = 0; i < n; i++)
 using namespace std;
 
 int N, M;
-int dxdy[4][3][2] = {
-        {{0, 1}, {1, 0}, {1, 1}}, 
-        {{1, 0}, {0, -1}, {1, -1}}, 
-        {{0, -1}, {-1, 0}, {-1, -1}}, 
-        {{0, 1}, {-1, 0}, {-1, 1}}
-    };
-bool visited[31][31] = {false, };
-int find(vector<string> board)
-{
-    int cnt = 0;
-    FOR(i, N)
-    {
-        FOR(j, M)
-        {
-            if(board[i][j] == '.') continue;
-            FOR(k, 4)
-            {
-                bool flag = false;
-                FOR(l, 3)
-                {
-                    int dx = i + dxdy[k][l][0];
-                    int dy = j + dxdy[k][l][1];
-                    if(dx < 0 || dy < 0 || dx >= N || dy >= M ||
-                        board[dx][dy] != board[i][j])
-                    {
-                        flag = true;
-                        break;
-                    }
-                }
-                if(!flag)
-                {
-                    FOR(l, 3)
-                    {
-                        int dx = i + dxdy[k][l][0];
-                        int dy = j + dxdy[k][l][1];
-                        if(!visited[dx][dy]) cnt++;
-                        visited[dx][dy] = true;
-                    }
-                    if(!visited[i][j]) cnt++;
-                    visited[i][j] = true;
-                }
-            }
-        }
-    }
-    return cnt;
-}
-
-void update(vector<string> &board)
-{
-    FOR(i, M)
-    {
-        bool flag = false;
-        for(int j = N-1; j >= 0; j--)
-        {
-            int cur = j;
-            if(visited[j][i] && !flag)
-                flag = true;
-
-            if(flag && !visited[j][i])
-            {
-                while(cur+1 < N && visited[cur+1][i])
-                {
-                    char tmp = board[cur][i];
-                    board[cur++][i] = '.';
-                    board[cur][i] = tmp; 
-                }
-
-                visited[cur][i] = false;
-            }
-        }
-    }
-}
-
+typedef pair<int, int> pii;
+priority_queue<pii, vector<pii>, greater<pii>> delete_info[MAX];
 void print(vector<string> board)
 {
     for(string str : board)
         cout << str << endl;
 }
-int solution(int m, int n, vector<string> board) {
-    int answer = 0;
-    N = m; M = n;
-
-    while(true)
+int isDestroy(vector<string> &board)
+{
+    int result = 0;
+    FOR(i, board.size() - 1)
     {
-        int cnt = find(board);
-        if(!cnt) break;
-        update(board);
-        answer += cnt;
-        memset(visited, false, sizeof(visited));
-        print(board);
+        if(board[i].length() < 0) continue;
+        cout << i << "/"<< board[i].length()<< endl;
+        FOR(j, board[i].length())
+        {
+            cout << "FUCK" << endl;
+            int cnt = 0;
+            int c = board[i][j];
+            for(int x = i; x < i + 2; x++)
+            {
+                for(int y = j; y < j + 2; y++)
+                {
+                    if(x < 0 || x >= board.size() || y < 0 || y >= board.size()) continue;
+                    if(board[x][y] == c)
+                        cnt++;
+                }
+            }
+
+            if(cnt == 4)
+            {
+                cout << i << "break!" << endl;
+                delete_info[i].push({j, j+1});
+                delete_info[i+1].push({j, j+1});
+                /*delete_info[i].first = min(delete_info[i].first, j);
+                delete_info[i].second = max(delete_info[i].second, j+1);
+                delete_info[i+1].first = min(delete_info[i+1].first, j);
+                delete_info[i+1].second = max(delete_info[i+1].second, j+1);*/
+                result++;
+            }
+        }
+    }
+    return result;
+}
+int solution(int m, int n, vector<string> board) {
+    N = n; M = m;
+    int answer = 0;
+    vector<string> tmpBoard;
+    for(int i = n-1; i >= 0; i--)
+    {
+        string tmp = "";
+        for(int j = m - 1; j >= 0; j--)
+            tmp += board[j][i];
+        tmpBoard.push_back(tmp);
     }
 
+    while(isDestroy(tmpBoard))
+    {
+        print(tmpBoard);
+        FOR(i, n)
+        {
+            int del_cnt = 0;
+            while(!delete_info[i].empty())
+            {  
+                int s = delete_info[i].top().first;
+                int e = delete_info[i].top().second;
+                delete_info[i].pop();
+
+                if(!delete_info[i].empty() && e + 1 >= delete_info[i].top().first)
+                {
+                    int e2 = delete_info[i].top().second;
+                    delete_info[i].pop();
+                    cout << "s : " << s << "e2 : " << e2 << endl;
+                    delete_info[i].push({s, e2});
+                    continue;
+                }
+                cout <<"delete[" << i << " ] : "<< s << "/" << e << endl;
+                tmpBoard[i].erase(tmpBoard[i].begin() + s - del_cnt, tmpBoard[i].begin() + e + 1 -(del_cnt));
+                answer += (e - s + 1); del_cnt += (e - s + 1);
+            }
+            /*cout << delete_info[i].first << "/" << delete_info[i].second << endl;
+            if(delete_info[i].first < MAX && delete_info[i].second > 0)
+            {
+                answer += (delete_info[i].second - delete_info[i].first + 1);
+                tmpBoard[i].erase(tmpBoard[i].begin() + delete_info[i].first, tmpBoard[i].begin() + delete_info[i].second+1);
+            }*/
+        }
+        cout << "=============" << endl;
+        
+    }
+    print(tmpBoard);
     return answer;
 }
-
 int main()
 {
     vector<string> board
         // = {"CCBDE", "AAADE", "AAABF", "CCBBF"};
+        // = {"AAAAAAAA", "BBAAAACC", "BBAAAACC"};
         // = {"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"};
-        = {"TTTANT",
+        /*= {"TTTANT",
            "RRFACC",
            "RRRFCC",
            "TMMRAA",
            "TTMMMF",
-           "TTMTTJ"};
-
-    cout << solution(6, 6, board);
+           "TTMTTJ"};*/
+    = {"DDABBAABBA",
+        "AAAAAABBBA",
+        "DDACCBBBAA",
+        "DDABBBBBAA",
+        "AAABBABBBA",
+        "CCADDAABBB",
+        "CCADDAABBB",
+        "BBACCABBBA",
+        "BBAAABBBAA",
+        "DDABBBBAAA"};
+    cout << solution(10, 10, board);
     return 0;
 }
