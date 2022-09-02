1884ef5062031bdf447d6cb98aa90d256293f1a1
keon-kim <srihweb@naver.com>
2022-09-02 19:01:32 +0900

    #20220902

--- level4/array.cpp
+++ level4/array.cpp
@@ -8,13 +8,15 @@
 #define RFOR(i, n) for(int i = n; i >= 0; i--)
 
 using namespace std;
+
 int R, C;
-void Shift_rc(int moveCnt, vector<vector<int>> &rc)
+vector<vector<int>> Shift_rc(int moveCnt, vector<vector<int>> &rc)
 {
-    int del = R - (moveCnt%R);
-    FOR(i, del)
-        rc.push_back(rc[i]);
-    rc.erase(rc.begin(), rc.begin()+del);
+    vector<vector<int>> res;
+    res.resize(R);
+    FOR(i, R) res[(i+moveCnt)%R] = rc[i];
+    
+    return res;
 }
 
 void Rotate_rc(int moveCnt, vector<vector<int>> &rc)
@@ -26,43 +28,37 @@ void Rotate_rc(int moveCnt, vector<vector<int>> &rc)
     for(int i = R-2; i >= 1; i--) res.push_back(rc[i][0]);
 
     int rnd = R*C - ((R-2) * (C-2));
-
-    FOR(i, rnd - moveCnt%rnd)
-        res.push_back(res[i]);
-
-    res.erase(res.begin(), res.begin() + (rnd - moveCnt%rnd));
-    
-    int cnt = 0;
-    FOR(i, C) rc[0][i] = res[cnt++];
-    FOR(i, R-1) rc[i+1][C-1] = res[cnt++];
-    RFOR(i, C-2) rc[R-1][i] = res[cnt++];
-    for(int i = R-2; i >= 1; i--) rc[i][0] = res[cnt++];
-    cout << cnt << "/" << rnd << endl;
+    int cnt = rnd - (moveCnt%rnd);
+    FOR(i, C) rc[0][i] = res[cnt++%rnd];
+    FOR(i, R-1) rc[i+1][C-1] = res[cnt++%rnd];
+    RFOR(i, C-2) rc[R-1][i] = res[cnt++%rnd];
+    for(int i = R-2; i >= 1; i--) rc[i][0] = res[cnt++%rnd];
 }
 
 
 vector<vector<int>> solution(vector<vector<int>> rc, vector<string> operations) {
-    vector<vector<int>> answer;
+    vector<string> operations_board;
     int cnt = 0; string cp = "X";
     R = rc.size(), C = rc[0].size();
-    vector<string> operations_board;
-    FOR(i, operations.size()){
+    
+    FOR(i, operations.size())
+    {  
         if(cp[0] != operations[i][0])
         {
-            operations_board.push_back(cp += (cnt+'0'));
+            operations_board.push_back(cp += to_string(cnt));
             cp = operations[i][0]; cnt = 0;
         }
         cnt++;
-        if(i == operations.size()-1) operations_board.push_back(cp+=(cnt+'0'));
+        if(i == operations.size()-1) operations_board.push_back(cp+=to_string(cnt));
     }
-
+    
     for(string str : operations_board)
     {
         if(str[0] == 'X') continue;
         if(str[0] == 'S')
         {
             str.erase(str.begin(), str.begin()+1);
-            Shift_rc(stoi(str), rc);
+            rc = Shift_rc(stoi(str), rc);
         }
         else
         {
@@ -70,7 +66,6 @@ vector<vector<int>> solution(vector<vector<int>> rc, vector<string> operations) 
             Rotate_rc(stoi(str), rc);
         }
     }
-
     return rc;
 }
 
@@ -82,8 +77,8 @@ int main()
         = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
     vector<string> operations
         // = {"Rotate", "ShiftRow"};
-        // = {"Rotate", "ShiftRow", "ShiftRow"};
-        = {"ShiftRow", "Rotate", "ShiftRow", "Rotate"};
+        = {"Rotate", "ShiftRow", "ShiftRow"};
+        // = {"ShiftRow", "Rotate", "ShiftRow", "Rotate"};
 
     vector<vector<int>> answer = solution(rc, operations);
     for(vector<int> v : answer)
