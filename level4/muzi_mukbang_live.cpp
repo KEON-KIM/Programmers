1088b9b7647424a68b93d48acca40bb44bff6ba1
keon-kim <srihweb@naver.com>
2022-09-10 16:55:33 +0900

    #20220910

---
+++ level4/muzi_mukbang_live.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> food_times, long long k) {
+    int answer = 0;
+    int s = food_times.size();
+    
+    return answer;
+}
+
+int main()
+{
+    vector<int> food_times = {3, 1, 2};
+    cout << solution(food_times, 5) << endl;
+    return 0;
+}
