178556bfa7410f8be7edb2df57be37a1bbcb4eb7
keon-kim <srihweb@naver.com>
2022-09-12 21:21:42 +0900

    #20220912

--- level4/muzi_mukbang_live.cpp
+++ level4/muzi_mukbang_live.cpp
@@ -1,24 +1,40 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <iostream>
-
+#include <algorithm>
+#include <queue>
+ 
 #define FOR(i, n) for(int i = 0; i < n; i++)
 using namespace std;
-
-typedef pair<int ,int> pii;
-int solution(vector<int> food_times, long long k) {
-    int answer = 0;
+ 
+typedef pair<int, int> pii;
+typedef long long ll;
+int solution(vector<int> food_times, ll k) {
+    ll cnt = 0, before = 0;
     priority_queue<pii, vector<pii>, greater<pii>> pQue;
-    FOR(i, food_times.size())
-        pQue.push({food_times[i], i});
     
-    while()
+    FOR(i, food_times.size())
     {
-        
+        cnt += food_times[i];
+        pQue.push({food_times[i], (i + 1)});
     }
 
-    return answer;
+    if (cnt <= k) return -1;
+    while ((pQue.top().first - before) * pQue.size() <= k) 
+    {
+        k -= (pQue.top().first - before) * pQue.size();
+        before = pQue.top().first;
+        pQue.pop();
+    }
+
+    vector<pii> res;
+    while (!pQue.empty()) 
+    {
+        res.push_back({pQue.top().second, pQue.top().first});
+        pQue.pop();
+    }
+    sort(res.begin(), res.end());
+    return res[k % res.size()].first;
 }
 
 int main()
