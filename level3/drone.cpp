e8f1c63f41978830b6951f2e2fc725bc388a5d15
keon-kim <srihweb@naver.com>
2022-11-06 23:10:56 +0900

    #20221106

---
+++ level3/drone.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board) {
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+    vector<vector<int>> board
+        = {{0, 0, 0, 1, 1},{0, 0, 0, 1, 0},{0, 1, 0, 1, 1},{1, 1, 0, 0, 1},{0, 0, 0, 0, 0}};
+    cout << solution(board);
+    return 0;
+}
